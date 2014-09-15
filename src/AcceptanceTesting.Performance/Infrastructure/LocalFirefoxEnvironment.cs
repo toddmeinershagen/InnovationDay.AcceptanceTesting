d2c097bf3d8cafb2c91da2e4dfb876bc95e619c8
Todd Meinershagen <todd@meinershagen.net>
2014-09-14 23:37:11 -0500

    Added a performance test project.

---
+++ src/AcceptanceTesting.Performance/Infrastructure/LocalFirefoxEnvironment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Bumblebee.Setup;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace AcceptanceTesting.Performance.Infrastructure
+{
+    public class LocalFirefoxEnvironment : IDriverEnvironment
+    {
+        public IWebDriver CreateWebDriver()
+        {
+            var binary = new FirefoxBinary(@"C:\Program Files (x86)\Mozilla Firefox\firefox.exe");
+            var driver = new FirefoxDriver(binary, new FirefoxProfile());
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(3));
+            return driver;
+        }
+    }
+}
---
+++ src/AcceptanceTesting.Performance/TaskFeature.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AcceptanceTesting.Common.Pages;
+using AcceptanceTesting.Performance.Infrastructure;
+using AcceptanceTesting.Specs.Infrastructure;
+using Bumblebee.Extensions;
+using Bumblebee.Setup;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AcceptanceTesting.Performance
+{
+    /// <summary>
+    /// Summary description for AddingATask
+    /// </summary>
+    [TestClass]
+    public class TaskFeature
+    {
+        private readonly Settings _settings = new Settings();
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestInitialize]
+        public void SetUp()
+        {  
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+        }
+
+        [TestMethod]
+        public void AddingATask()
+        {
+            Action action = () =>
+            {
+                //var taskInfo = new {Name = "Task_" + Guid.NewGuid(), Note = "Note for task."};
+
+                Threaded<Session>
+                    .With<LocalFirefoxEnvironment>()
+                    .NavigateTo<LoggedOutPage>(_settings.BaseUrl)
+                    .LoginArea
+                    .Username.EnterText(_settings.ValidUserName)
+                    .Password.EnterText(_settings.ValidPassword)
+                    .Login.Click<LoggedInPage>();
+
+                Threaded<Session>
+                    .End();
+            };
+
+            action();
+        }
+    }
+}
